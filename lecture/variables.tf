d00ed41f2c21d22fb5a095034ef887180503f106
Frank Fichtenmüller <frank.fichtenmueller@sclable.com>
2020-02-10 10:07:37 +0100

    brake out variables from variables.tf to .tfvariables

--- lecture/variables.tf
+++ lecture/variables.tf
@@ -3,29 +3,13 @@ variable "env" {
 }
 variable "image" {
   type = map
-  default = {
-    dev = "ghost:latest"
-    prod = "ghost:alpine"
-  }
 }
 variable "container_name" {
   type = map
-  default = {
-    dev = "dev_blog"
-    prod = "prod_blog"
-  }
 }
 variable "int_port" {
   type = map 
-  default = {
-    dev = "2368"
-    prod = "2368"
- }
 }
 variable "ext_port" {
   type = map
-  default = {
-    dev = "80"
-    prod = "8080"
-  }
 }
